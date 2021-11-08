a7b08766f7347dc886f73c99a0697044f469f4ca
jaenwawe <nwawe.jae@gmail.com>
2021-11-07 22:17:33 -0600

    components log and navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,38 @@
-import logo from './logo.svg';
+import { useEffect } from "react";
+import logo from './logo.png';
 import './App.css';
+import NavBar from './NavBar';
+import {username, password,image} from "./Mock"
+
 
 function App() {
+
+  useEffect(() => {
+    fetch("/users")
+      .then((r) => r.json())
+      .then((movies) => console.log(movies));
+  }, []);
+
+
+ 
+  
+
+
+
   return (
     <div className="App">
+ 
       <header className="App-header">
+      <NavBar />
+    
+ 
+     
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+  
+    
       </header>
     </div>
   );
---
+++ client/src/Mock.js
@@ -0,0 +1,8 @@
+
+
+const username = "antonette";
+const password = "abc123";
+const image = "https://i.imgur.com/mV8PQxj.gif";
+
+// export all three variables as named exports
+export { username, password, image };
